deb1369e49643339972136f06521efc80ed0c990
Глеб <stroikagoda@yandex.ru>
2026-07-19 01:13:23 +0400

    Ьлочный комментарий с минусом

---
+++ int.java
@@ -0,0 +1,28 @@
+int x=1;
+int y=x*2;
+int z=5*y*y+2*y+3;
+
+int a=5;
+int b=1;
+int c= (a-b)*(a+b);
+
+String s = "Amigo";
+String s = "123";
+String s = "Bond007";
+
+String s = "Amigo" + "the best";
+String s = "";
+int x = 333;
+String s = "Amigo" + x;
+
+System.out.println("Amigo");
+System.out.println("Ami"+"go");
+String s = "Amigo";
+System.out.println(s);
+String s = "Am";
+System.out.println(s + "igo");
+
+/* Многострочный комментарий */
+// однострочный комментарий
+
+/*- Блочный комментарий, из-за минуса его текст отредактирован не будет */
