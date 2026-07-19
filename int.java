e2b7629826a4e7398773adb70d1ee9a04de12aaa
Глеб <stroikagoda@yandex.ru>
2026-07-19 21:53:15 +0400

    Алиасы - псевдонимы для данных в таблице

--- int.java
+++ int.java
@@ -1,28 +1,9 @@
-int x=1;
-int y=x*2;
-int z=5*y*y+2*y+3;
+sql
+describe Название таблицы
 
-int a=5;
-int b=1;
-int c= (a-b)*(a+b);
+SELECT * FROM Название таблицы
 
-String s = "Amigo";
-String s = "123";
-String s = "Bond007";
+SELECT Memberid, Membername
+FROM Название таблицы 
 
-String s = "Amigo" + "the best";
-String s = "";
-int x = 333;
-String s = "Amigo" + x;
-
-System.out.println("Amigo");
-System.out.println("Ami"+"go");
-String s = "Amigo";
-System.out.println(s);
-String s = "Am";
-System.out.println(s + "igo");
-
-/* Многострочный комментарий */
-// однострочный комментарий
-
-/*- Блочный комментарий, из-за минуса его текст отредактирован не будет */
+SELECT member_id, member_name AS Name FROM FamilyMembers
