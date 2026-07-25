58e4bab9c8fdb8e3ebe8bbe7cccb5b26426e92a4
Глеб <stroikagoda@yandex.ru>
2026-07-26 00:36:29 +0400

    Коммит 26 июля

--- int.java
+++ int.java
@@ -7,3 +7,22 @@ SELECT Memberid, Membername
 FROM Название таблицы 
 
 SELECT member_id, member_name AS Name FROM FamilyMembers
+
+SELECT member_id, member_name Name FROM FamilyMembers
+
+SELECT 'Строка Другая строка' as String
+
+SELECT 'Cтрока \n Другая строка' as String
+
+%, MOD Деление по модулю 11%5=1
+DIV Целочисленное деление 10 DIV 4 = 2
+SELECT (5*2-6)/2 AS Result
+
+SELECT * FROM FamilyMembers WHERE birthday > '1970-12-30'
+
+SELECT UPPER('Hello world') AS upper_string;
+
+
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+}
