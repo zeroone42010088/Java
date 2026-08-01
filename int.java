976e914ac43da91e4f9f62fb76d60c5bbe70b53d
Глеб <stroikagoda@yandex.ru>
2026-08-02 01:01:19 +0400

    Коммит 2-08-26

--- int.java
+++ int.java
@@ -26,3 +26,31 @@ SELECT UPPER('Hello world') AS upper_string;
 public static void main(String[] args) {
     System.out.println("Hello World!");
 }
+
+int x,y,z;
+int a;
+a = 5;
+int a=7; int b=4; int c=5;
+
+int a (2+2)*2;
+int b = (6-3)/(9-6);
+int c = (-2)*(-3);
+int d = 3/0;
+int a = 5/2;
+int a=b%2;
+int b=20%4;
+int a=5%2; a=1
+int b=20%4; b=0;
+int c=9%5; с=4
+
+(a%2)==0
+a++;
+a--;
+
+SELECT * FROM animals
+SELECT name FROM animals
+
+int x = 5;
+x = x+1; значение будет 6
+x = x+1; значение будет 7
+x = x+1; значение будет 8
