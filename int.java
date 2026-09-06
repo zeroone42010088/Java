5bb7c62d33809b2dda0ce184bd79ac01d2b06ae4
Глеб <stroikagoda@yandex.ru>
2026-09-06 22:14:35 +0400

    Коммит 06-09-26

--- int.java
+++ int.java
@@ -153,3 +153,5 @@ ORDER BY category ASC, price DESC
 LIMIT 10;       
 
 String nameShort = name.trim(); String lowercity = city.toLowerCase(); String upperprofession = profession.toUpperCase(); System.out.print("Пользователь:" + nameShort + "\n" + "Город:" + lowercity + "\n" + "Профессия:" + upperprofession );
+
+SELECT name, category, price, stock FROM products WHERE category IN ('Книги', 'Спорт', 'Прочее') AND price BETWEEN 300 and 2000 AND stock = "0" ORDER BY category ASC, price DESC LIMIT 10;
